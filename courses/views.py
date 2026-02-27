3ba9d4a5f22fe3f72e777e1d106076a4ffce8edb
jagadeesh <jagadeeshbabu659@gmail.com>
2026-02-27 16:42:01 +0530

    Added submit and show_exam_result views and urls

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice, Submission
 
-# Create your views here.
+
+def submit(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+
+    if request.method == "POST":
+        selected_choice_id = request.POST.get("choice")
+        selected_choice = get_object_or_404(Choice, id=selected_choice_id)
+
+        Submission.objects.create(
+            question=question,
+            selected_choice=selected_choice
+        )
+
+        return render(request, "result.html", {
+            "question": question,
+            "selected_choice": selected_choice
+        })
+
+    return render(request, "submit.html", {"question": question})
+
+
+def show_exam_result(request):
+    submissions = Submission.objects.all()
+    return render(request, "exam_result.html", {
+        "submissions": submissions
+    })
