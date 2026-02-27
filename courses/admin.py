88d31b840d0d660632eb2a337b1ef5ac19febb1c
jagadeesh <jagadeeshbabu659@gmail.com>
2026-02-27 16:28:58 +0530

    Added Django project files

---
+++ courses/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Question, Choice, Submission
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
